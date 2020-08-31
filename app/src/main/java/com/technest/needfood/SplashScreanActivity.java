af6f8241b60ccacb040fd7401b87ee2842b3b083
azwarbahar07 <azwarbahar07@gmail.com>
2020-08-31 19:40:14 +0800

    Splash Update

---
+++ app/src/main/java/com/technest/needfood/SplashScreanActivity.java
@@ -0,0 +1,14 @@
+package com.technest.needfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screan);
+    }
+}
