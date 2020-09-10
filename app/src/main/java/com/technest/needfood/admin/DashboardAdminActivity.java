7c91eb0324c30a4a486072ae144e91d2a2343b20
azwarbahar07 <azwarbahar07@gmail.com>
2020-09-10 16:23:52 +0800

    Update Sliding Menu

--- app/src/main/java/com/technest/needfood/admin/DashboardAdminActivity.java
+++ app/src/main/java/com/technest/needfood/admin/DashboardAdminActivity.java
@@ -2,15 +2,82 @@ package com.technest.needfood.admin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
 
+import com.special.ResideMenu.ResideMenu;
+import com.special.ResideMenu.ResideMenuItem;
 import com.technest.needfood.R;
 
-public class DashboardAdminActivity extends AppCompatActivity {
+public class DashboardAdminActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ResideMenu resideMenu;
+    private Context mContext;
+    private ResideMenuItem itemHome;
+    private ResideMenuItem itemPesanan;
+    private ResideMenuItem itemStokBahan;
+    private ResideMenuItem itemInventori;
+    private ResideMenuItem itemKeuangan;
+    private ResideMenuItem itemSetting;
+    private ResideMenuItem itemLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_admin);
+
+        mContext = this;
+
+        setUpMenu();
+
+    }
+
+    private void setUpMenu() {
+
+        resideMenu = new ResideMenu(this);
+        resideMenu.setBackground(R.drawable.bg_main);
+        resideMenu.attachToActivity(this);
+        resideMenu.setMenuListener(menuListener);
+        resideMenu.setScaleValue(0.6f);
+        resideMenu.setSwipeDirectionDisable(0);
+        resideMenu.setSwipeDirectionDisable(1);
+
+        itemHome     = new ResideMenuItem(this, R.drawable.ic_android_black_24dp, "  Home");
+
+        itemHome.setOnClickListener(this);
+        resideMenu.addMenuItem(itemHome, ResideMenu.DIRECTION_LEFT);
+
+        findViewById(R.id.title_bar_left_menu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resideMenu.openMenu(ResideMenu.DIRECTION_LEFT);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        return resideMenu.dispatchTouchEvent(ev);
+    }
+
+    private ResideMenu.OnMenuListener menuListener = new ResideMenu.OnMenuListener() {
+        @Override
+        public void openMenu() {
+            Toast.makeText(mContext, "Menu is opened!", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void closeMenu() {
+            Toast.makeText(mContext, "Menu is closed!", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
