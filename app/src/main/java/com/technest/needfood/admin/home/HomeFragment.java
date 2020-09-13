1b7aa5537c08d44176c7f4994c68bbc825c9d3b5
azwarbahar07 <azwarbahar07@gmail.com>
2020-09-13 21:49:25 +0800

    tambah aksi buttom di home dan pesanan

--- app/src/main/java/com/technest/needfood/admin/home/HomeFragment.java
+++ app/src/main/java/com/technest/needfood/admin/home/HomeFragment.java
@@ -7,18 +7,94 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.technest.needfood.R;
+import com.technest.needfood.admin.dompet.DompetFragment;
+import com.technest.needfood.admin.inventori.InventoriFragment;
+import com.technest.needfood.admin.pesanan.PesananFragment;
+import com.technest.needfood.admin.stok.StokFragment;
 
 public class HomeFragment extends Fragment {
 
     View v;
 
+    private CardView cv_pesanan;
+    private CardView cv_stok;
+    private CardView cv_inventory;
+    private CardView cv_keuangan;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // aksi tombol pesanan
+        cv_pesanan = v.findViewById(R.id.cv_pesanan);
+        cv_pesanan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Fragment pesananFragment = new PesananFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.main_fragment, pesananFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+            }
+        });
+
+        // aksi tombol stok
+        cv_stok = v.findViewById(R.id.cv_stok);
+        cv_stok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Fragment stokFragment = new StokFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.main_fragment, stokFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+            }
+        });
+
+        // aksi tombol inventory
+        cv_inventory = v.findViewById(R.id.cv_inventory);
+        cv_inventory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Fragment inventoryFragment = new InventoriFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.main_fragment, inventoryFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+            }
+        });
+
+        // aksi tombol keuangan
+        cv_keuangan = v.findViewById(R.id.cv_keuangan);
+        cv_keuangan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Fragment keuanganFragment = new DompetFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.main_fragment, keuanganFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+            }
+        });
 
         return  v;
     }
--- app/src/main/java/com/technest/needfood/admin/pesanan/PesananFragment.java
+++ app/src/main/java/com/technest/needfood/admin/pesanan/PesananFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.technest.needfood.R;
@@ -15,10 +17,73 @@ public class PesananFragment extends Fragment {
 
     View v;
 
+    private CardView cv_pesanan_pending;
+    private CardView cv_pesanan_lunsa;
+    private CardView cv_pesanan_dapur;
+    private CardView cv_pesanan_delivery;
+    private CardView cv_pesanan_sampai;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.fragment_pesanan, container, false);
+
+        // aksi tombol pesanan pending
+        cv_pesanan_pending = v.findViewById(R.id.cv_pesanan_pending);
+        cv_pesanan_pending.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Klik Buttom Pesanan Pending!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // aksi tombol pesanan lunas
+        cv_pesanan_lunsa = v.findViewById(R.id.cv_pesanan_lunsa);
+        cv_pesanan_lunsa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getActivity(), "Klik Buttom Pesanan Lunas!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // aksi tombol proses dapur
+        cv_pesanan_dapur = v.findViewById(R.id.cv_pesanan_dapur);
+        cv_pesanan_dapur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getActivity(), "Klik Buttom Proses Dapur!",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        // aksi tombol pesanan delivery
+        cv_pesanan_delivery = v.findViewById(R.id.cv_pesanan_delivery);
+        cv_pesanan_delivery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getActivity(), "Klik Buttom Pesanan Delivery!",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        // aksi tombol pesanan sampau
+        cv_pesanan_sampai = v.findViewById(R.id.cv_pesanan_sampai);
+        cv_pesanan_sampai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getActivity(), "Klik Buttom Pesanan Sampai!",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
         return  v;
     }
